cd8eee1da94d792ea84a4149a0e07770c0e557a4
Philipp Keese <philipp.keese@greyld.com>
2022-04-18 17:39:09 +0200

    add MemoryLayer interface

---
+++ memory_layer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "memory_device.hpp"
+
+namespace rambock {
+
+/** Abstract Memory Layer
+ * Requires an underlying memory device to work on.
+ * Sub-classes need to implement read/write
+ */
+struct MemoryLayer : public MemoryDevice {
+  protected:
+	MemoryLayer(MemoryDevice &memory);
+
+	inline MemoryDevice &memory() const { return m_memory; }
+
+  private:
+	MemoryDevice &m_memory;
+};
+
+MemoryLayer::MemoryLayer(MemoryDevice &memory) : m_memory{memory} {}
+
+} // namespace rambock
